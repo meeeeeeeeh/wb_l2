86ef80b178d72c1d889dc4dea2884c04726d25d0
polina <you@example.com>
2024-05-02 18:21:03 +0300

    task 10

--- develop/dev10/task.go
+++ develop/dev10/task.go
@@ -15,8 +15,95 @@ go-telnet --timeout=10s host port go-telnet mysite.ru 8080 go-telnet --timeout=3
 программа должна завершаться через timeout
 */
 
+//вначале вызывается netcat для создания сервера кторый будет прослушивать определенный порт
+// nc -lp 1234
+
 package main
 
-func main() {
+import (
+	"bufio"
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"time"
+)
 
+type client struct {
+	time time.Duration
+	host string
+	port string
+}
+
+func newClient() (*client, error) {
+	//парсит строку и ищет там лаг timeout, если не будет найден то знавение 10сек
+	time := flag.Duration("timeout", 10*time.Second, "getting time of server working")
+	flag.Parse()
+
+	if len(os.Args) < 2 {
+		err := errors.New("host or port name wasn't specified")
+		return nil, err
+	}
+
+	//возвращает аргументы командной строки котрык не являются флагами
+	args := flag.Args()
+
+	return &client{
+		time: *time,
+		host: args[0],
+		port: args[1],
+	}, nil
+}
+
+func newConnection() error {
+	client, err := newClient()
+	if err != nil {
+		return err
+	}
+
+	connect, err := net.DialTimeout("tcp", client.host+":"+client.port, client.time)
+	if err != nil {
+		return err
+	}
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, client.time)
+	defer cancel()
+
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Connection time is out")
+			connect.Close()
+			return nil
+		default:
+			//ввод из stdin записывается в соединение
+			reader := bufio.NewReader(os.Stdin)
+			text, err := reader.ReadString('\n')
+			if err != nil {
+				return err
+			}
+			fmt.Fprintln(connect, text)
+
+			//ввод из конекта записывается в stdin
+			reader = bufio.NewReader(connect)
+			text, err = reader.ReadString('\n')
+			if err != nil {
+				return err
+			}
+			fmt.Println(text)
+
+		}
+	}
+
+}
+
+func main() {
+	err := newConnection()
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
